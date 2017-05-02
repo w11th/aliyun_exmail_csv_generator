eb69c2b9134ab9f5ad5ca803bbbdfa607507347a
Wenhui Wang <w.eleventh@gmail.com>
2017-05-02 15:55:34 +0800

    Fix csv encoding

--- lib/aliyun_exmail_csv_generator/exporter.rb
+++ lib/aliyun_exmail_csv_generator/exporter.rb
@@ -9,7 +9,7 @@ module AliyunExmailCSVGenerator
         filepath = File.absolute_path(File.expand_path(filepath))
         filedir = File.dirname filepath
         FileUtils.mkdir_p filedir unless File.exist? filedir
-        CSV.open(filepath, 'w:UTF-8:GB2312') do |csv|
+        CSV.open(filepath, 'w:GB2312') do |csv|
           csv << ["#姓名", "邮件地址", "工号", "密码", "部门", "分机", "职称", "手机号", "容量"]
           accounts.each do |account|
             csv << account.to_csv_row
