efcb6fe04118cee60405135e021c7fe086ba256c
Wenhui Wang <w.eleventh@gmail.com>
2017-05-03 14:35:50 +0800

    update accout_spec

--- spec/aliyun_exmail_csv_generator/account_spec.rb
+++ spec/aliyun_exmail_csv_generator/account_spec.rb
@@ -5,38 +5,35 @@ module AliyunExmailCSVGenerator
   describe Account do
     context '#initialize' do
       context 'without options' do
-        before do
-          @account = Account.new('David', 'david@qcpt.org')
-        end
+        let(:account) { Account.new('David', 'david@qcpt.org') }
         it 'has default password' do
-          expect(@account.password).to eq(Account::DEFAULT_OPTIONS[:password])
+          expect(account.password).to eq(Account::DEFAULT_OPTIONS[:password])
         end
 
         it 'has default department' do
-          expect(@account.department).to eq(Account::DEFAULT_OPTIONS[:department])
+          expect(account.department).to eq(Account::DEFAULT_OPTIONS[:department])
         end
 
         it 'has default capacity' do
-          expect(@account.capacity).to eq(Account::DEFAULT_OPTIONS[:capacity])
+          expect(account.capacity).to eq(Account::DEFAULT_OPTIONS[:capacity])
         end
       end
 
       context 'with options' do
-        before do
-          @options = {password: 'override', department: 'new department', mobile: '12345', capacity: 1024}
-        end
-        it 'overrides the default option' do
-          account = Account.new('test', 'test@wang.org', @options)
+        let(:options) { {password: 'override', department: 'new department', mobile: '12345', capacity: 1024} }
 
-          expect(account.password).to eq(@options[:password])
-          expect(account.department).to eq(@options[:department])
-          expect(account.mobile).to eq(@options[:mobile])
-          expect(account.capacity).to eq(@options[:capacity])
+        it 'overrides the default option' do
+          account = Account.new('test', 'test@wang.org', options)
+
+          expect(account.password).to eq(options[:password])
+          expect(account.department).to eq(options[:department])
+          expect(account.mobile).to eq(options[:mobile])
+          expect(account.capacity).to eq(options[:capacity])
         end
 
         it 'should be default if not specific' do
-          @options[:department] = nil
-          account = Account.new('test', 'test@wang.org', @options)
+          options[:department] = nil
+          account = Account.new('test', 'test@wang.org', options)
           expect(account.department).to eq(Account::DEFAULT_OPTIONS[:department])
         end
       end
@@ -50,26 +47,23 @@ module AliyunExmailCSVGenerator
     end
 
     context '.name_to_pinyin' do
-      before do
-        @name_list = %w(张三 李四 王五一 David\ Bowie)
-        @name_pinyin_list = ['san.zhang', 'si.li', 'wuyi.wang', 'bowie.david']
-      end
+      let(:name_list) { %w(张三 李四 王五一 David\ Bowie) }
+      let(:name_pinyin_list) { ['san.zhang', 'si.li', 'wuyi.wang', 'bowie.david'] }
 
       it 'should transform name to pinyin' do
-        @name_list.each_with_index do |name, index|
-          expect(Account.name_to_pinyin(name)).to eq(@name_pinyin_list[index])
+        name_list.each_with_index do |name, index|
+          expect(Account.name_to_pinyin(name)).to eq(name_pinyin_list[index])
         end
       end
     end
 
     context '.from' do
-      before do
-        @name_list = %w(张三 李四 王五一 David\ Bowie)
-      end
+      let(:name_list) { %w(张三 李四 王五一 David\ Bowie) }
       it 'should transform name list to account list' do
-        accounts = Account.from(@name_list)
+        accounts = Account.from(name_list)
         accounts.each_with_index do |account, index|
-          expect(account.name).to eq(@name_list[index])
+          expect(account).to a_kind_of(Account)
+          expect(account.name).to eq(name_list[index])
         end
       end
     end
